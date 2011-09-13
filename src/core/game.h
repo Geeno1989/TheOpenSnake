989cb8f866e5c24f5a2da168cb228613e3f26c79
Francesco Gaudenzi <fgaudo@gmail.com>
2011-09-13 20:00:14 +0200

    Added const qualifiers to function parameters

--- src/core/game.h
+++ src/core/game.h
@@ -35,7 +35,7 @@ typedef struct Game {
 
 /*
   Parameter:    None
-  Returns:      A Game struct instance
+  Returns:      An instance of a Game Struct
   Description:  This function initializes the instance and sets all default parameters
 */
 Game*
@@ -44,114 +44,114 @@ init_game();
 
 
 /*
-  Parameter:    A Game struct instance 
+  Parameter:    A constant instance of a constant Game Struct
   Returns:      Nothing
   Description:  This function deallocates the Game struct instance
 */
 void
-free_game(Game* game);
+free_game(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      Nothing
   Description:  Resets the instance to default values;
 */
 void
-reset_game(Game* game);
+reset_game(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      Nothing
   Description:  Starts the Game and stores the current time in game->timeStart
 */
 void
-start_game(Game* game);
+start_game(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      Nothing
   Description:  Ends the Game and stores the current time in game->timeEnd
 */
 void
-end_game(Game* game);
+end_game(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a constant Game Struct
   Returns:      The duration of the game expressed in time_t
   Description:  Calculates the duration of the game
                 from game->timeStart to game->timeStop
 */
 time_t
-get_duration(Game* game);
+get_duration(const Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a constant Game Struct
   Returns:      Number of lives left
   Description:  Return the number of lives left from game->lives
 */
 int
-get_lives(Game* game);
+get_lives(const Game* const game);
 
 
 /*
-  Parameter:    A Game struct instance, Points expressed in Int
+  Parameter:    A constant instance of a Game Struct, A constant value expressed in int
   Returns:      Nothing
   Description:  Adds points to game->points
 */
 void
-add_points(Game* game, int points);
+add_points(Game* const game, const int points);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a constant Game Struct
   Returns:      Points expressed in int
   Description:  Returns the points from game->points
 */
 int
-get_points(Game* game);
+get_points(const Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      0 if maximum of lives ha been reached
                 1 if succesfully added the life
   
   Description:  Adds 1 life to game->lives
 */
 int
-add_life(Game* game);
+add_life(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      0 if there are no more lives to remove (the snake is dead)
                 1 if succesfully removed the life
   
   Description:  Removes 1 life from game->lives
 */
 int
-remove_life(Game* game);
+remove_life(Game* const game);
 
 
 
 /*
-  Parameter:    A Game struct instance
+  Parameter:    A constant instance of a Game Struct
   Returns:      Nothing
   Description:  Adds 1 snake part to game->snakeParts
 */
 void
-add_snakePart(Game* game);
+add_snakePart(Game* const game);
 
